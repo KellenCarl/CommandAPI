42b9ecd612e7e103ce373bdc35d7c7b86fdcee87
Kellen Carl <kellen.carl@gmail.com>
2020-09-06 16:27:48 -0500

    Added Controller Action Get Single Resource and Corresponding Tests

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -18,6 +18,16 @@ namespace CommandAPI.Controllers
         {
             return _context.CommandItems;
         }
+
+        //GET: api/commands/{Id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+            if(commandItem == null)
+                return NotFound();
+            return commandItem;
+        }
     }
 
 }
