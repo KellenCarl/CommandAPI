17634229d8e08a68235658ee897e31e280d04158
Kellen Carl <kellen.carl@gmail.com>
2020-09-30 19:48:57 -0500

    Removed connection string elements from appsettingss

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using CommandAPI.Models;
 using Npgsql;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 
 namespace CommandAPI
 {
@@ -20,7 +16,7 @@ namespace CommandAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public IConfiguration Configuration {get;}
-        public Startup (IConfiguration configuration) => 
+        public Startup(IConfiguration configuration) => 
             Configuration = configuration;
 
         public void ConfigureServices(IServiceCollection services)
